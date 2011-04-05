79216cf7c3351ed6606a0e46c4653d1283f571a4
Lorenz Moesenlechner <moesenle@in.tum.de>
2011-04-05 23:06:26 +0200

    Set file encoding for post mode

--- init.el
+++ init.el
@@ -14,6 +14,10 @@
 (require 'post)
 ;; use cool ldap-search and mutt aliases and addressbook for composing mails
 (require 'mail-addons)
+(add-hook 'post-mode-hook (lambda ()
+                            (interactive)
+                            (set-buffer-file-coding-system 'raw-text)))
+
 
 ;; Emacs should always ask for confirmation on exit
 (setq confirm-kill-emacs 'yes-or-no-p)
